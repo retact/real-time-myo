878f46745e02ba2aed56d643652cd967ca7b8777
takutosoeda <003starrysky@gmail.com>
2021-09-23 18:01:01 +0900

    [update] add 3d show

--- realtime-viewer.py
+++ realtime-viewer.py
@@ -21,46 +21,54 @@ def CreateArray(n, m):
 
 
 def proc_emg1(timestamp, emg, moving, characteristic_num, times=[]):
-    #print('Myo1 wrote')
-    #print(emg)
-    #print(time.time())
-    emg_data[:-1,:8] = emg_data[1:,:8]
-    emg_data[-1,:8] = emg
+    # print('Myo1 wrote')
+    # print(emg)
+    # print(time.time())
+    emg_data[:-1, :8] = emg_data[1:, :8]
+    emg_data[-1, :8] = emg
 
 
 def proc_emg2(timestamp, emg, moving, characteristic_num, times=[]):
-    #print('Myo2 wrote')
-    #print(emg)
-    #print(time.time())
+    # print('Myo2 wrote')
+    # print(emg)
+    # print(time.time())
     emg_data[:-1, 8:] = emg_data[1:, 8:]
     emg_data[-1, 8:] = emg
 
 
 def viewer(emg_data):
     fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-    #ax2 = fig.add_subplot(212)
+    ax1 = fig.add_subplot(211)
+    # ax2 = fig.add_subplot(212)
     fig.subplots_adjust(hspace=0.6)
-    ax1.set(xlim=[0,window_size],ylim=[-128,127])
-    #ax2.set(xlim=[0,window_size],ylim=[-128,127])
-    ax1.set_title('Myo Armband 1',fontsize=15)
-    #ax2.set_title('Myo Armband 2',fontsize=15)
-    ax1.set_ylabel('EMG signals',fontsize=12)
-    #ax2.set_ylabel('EMG signals',fontsize=12)
-    ax1.set_xlabel('Number of Data',fontsize=12)
-    #ax2.set_xlabel('Number of Data',fontsize=12)
+    ax1.set(xlim=[0, window_size], ylim=[-128, 127])
+    # ax2.set(xlim=[0,window_size],ylim=[-128,127])
+    ax1.set_title('Myo Armband 1', fontsize=15)
+    # ax2.set_title('Myo Armband 2',fontsize=15)
+    ax1.set_ylabel('EMG signals', fontsize=12)
+    # ax2.set_ylabel('EMG signals',fontsize=12)
+    ax1.set_xlabel('Number of Data', fontsize=12)
+    # ax2.set_xlabel('Number of Data',fontsize=12)
+    ax2 = fig.add_subplot(212, projection="3d")
 
     signals = []
+    3dsignals = []
+
     for i in range(8):
-        signal, = ax1.plot(np.arange(window_size),np.zeros(window_size),alpha=0.4)
+        signal, = ax1.plot(np.arange(window_size), np.zeros(window_size), alpha=0.4)
         signals.append(signal)
+        3dsignal, = ax2.plot(np.arange(windowsize), np.full_like(window_size, i+1),
+                             np.zeros(window_size))
     #for i in range(8):
     #    signal, = ax2.plot(np.arange(window_size),np.zeros(window_size),alpha=0.4)
     #    signals.append(signal)
 
     def update(i):
-        for i, signal in enumerate(signals):
-            signal.set_ydata(emg_data[:,i])
+        for count_1, signal in enumerate(signals):
+            signal.set_ydata(emg_data[:, count_1])
+        for count_2, 3dsignal in enumrate(3dsignals):
+            3dsignal.set_ydata(np.full_like(window_size, count_2+1))
+            3dsignal.set_3dproperties(emg_data[:, count_2])
 
     ani = FuncAnimation(fig,update,interval=100)
     plt.show()
